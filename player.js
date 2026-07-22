6399b350bc6bb115dd52fc61d3298dd0a812ff5b
Trapgad <davidedemquarshie1@gmail.com>
2026-07-22 06:47:08 +0000

    Update player.js

--- player.js
+++ player.js
@@ -1,10 +1,12 @@
 // =================================
 // TRAP CITY 3D
-// PLAYER MOVEMENT SYSTEM V1
+// PLAYER MOVEMENT V1
 // =================================
 
 
 export const keys = {};
+
+
 
 let velocity = {
     x:0,
@@ -12,86 +14,111 @@ let velocity = {
 };
 
 
-window.addEventListener("keydown",(event)=>{
 
-    keys[event.key.toLowerCase()] = true;
+window.addEventListener(
+"keydown",
+(e)=>{
 
-});
-
-
-window.addEventListener("keyup",(event)=>{
-
-    keys[event.key.toLowerCase()] = false;
+keys[
+e.key.toLowerCase()
+]=true;
 
 });
 
 
 
-export function updatePlayer(player, delta){
+window.addEventListener(
+"keyup",
+(e)=>{
 
+keys[
+e.key.toLowerCase()
+]=false;
 
-    const speed = 5;
-
-
-    let direction = {
-        x:0,
-        z:0
-    };
-
-
-    // Movement input
-
-    if(keys["w"])
-        direction.z -= 1;
-
-
-    if(keys["s"])
-        direction.z += 1;
-
-
-    if(keys["a"])
-        direction.x -= 1;
-
-
-    if(keys["d"])
-        direction.x += 1;
+});
 
 
 
-    // Normalize diagonal movement
-
-    const length = Math.sqrt(
-        direction.x ** 2 +
-        direction.z ** 2
-    );
 
 
-    if(length > 0){
+export function updatePlayer(
+player,
+delta
+){
 
-        direction.x /= length;
-        direction.z /= length;
 
-    }
+const speed = 5;
 
 
 
-    // Smooth movement
-
-    velocity.x +=
-    (direction.x * speed - velocity.x) * 0.15;
-
-
-    velocity.z +=
-    (direction.z * speed - velocity.z) * 0.15;
+let direction = {
+    x:0,
+    z:0
+};
 
 
 
-    player.position.x +=
-    velocity.x * delta;
+// Controls
+
+if(keys["w"])
+direction.z -= 1;
 
 
-    player.position.z +=
-    velocity.z * delta;
+if(keys["s"])
+direction.z += 1;
+
+
+if(keys["a"])
+direction.x -= 1;
+
+
+if(keys["d"])
+direction.x += 1;
+
+
+
+
+// Normalize movement
+
+const length =
+Math.sqrt(
+direction.x ** 2 +
+direction.z ** 2
+);
+
+
+
+if(length > 0){
+
+direction.x /= length;
+direction.z /= length;
+
+}
+
+
+
+
+// Smooth movement
+
+velocity.x +=
+(direction.x * speed - velocity.x)
+* 0.15;
+
+
+velocity.z +=
+(direction.z * speed - velocity.z)
+* 0.15;
+
+
+
+
+player.position.x +=
+velocity.x * delta;
+
+
+
+player.position.z +=
+velocity.z * delta;
 
 
 }
