43172d124737ff6277e4722d484463221fb9138d
Trapgad <davidedemquarshie1@gmail.com>
2026-07-22 06:44:03 +0000

    Create camera.js

---
+++ camera.js
@@ -0,0 +1,55 @@
+// =================================
+// TRAP CITY 3D
+// CAMERA SYSTEM V1
+// =================================
+
+
+export function updateCamera(
+    camera,
+    player
+){
+
+
+const distance = 10;
+
+const height = 7;
+
+
+// Target camera position
+
+const targetX =
+player.position.x;
+
+
+const targetY =
+player.position.y + height;
+
+
+const targetZ =
+player.position.z + distance;
+
+
+
+// Smooth movement
+
+camera.position.x +=
+(targetX - camera.position.x) * 0.08;
+
+
+camera.position.y +=
+(targetY - camera.position.y) * 0.08;
+
+
+camera.position.z +=
+(targetZ - camera.position.z) * 0.08;
+
+
+
+// Look at player
+
+camera.lookAt(
+player.position
+);
+
+
+}
